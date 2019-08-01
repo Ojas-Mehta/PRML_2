ee88c5920d1410894686f7f85d85fb810fa0d0a0
Ojas Mehta <ojasmehta189@gmail.com>
2019-08-01 15:01:23 +0530

    Delete covariance.m

--- src_assign2_group5/src_assign2_group5/covariance.m
+++
@@ -1,9 +0,0 @@
-function c = covariance(X)
-[row,col] = size(X);
-mean_vector = zeros(1,col);
-for i=1:col
-    mean_vector(:,i) = mean(X(:,i));
-end
-c = bsxfun(@minus,X,mean_vector);
-c = (c.'*c)/(row-1);
-end
